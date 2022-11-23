87d78fe3aa5d71986e763eeea2e31163b3f8cd0e
jimmyjetter27 <jimmyjetter27@gmail.com>
2022-11-23 13:13:48 +0000

    available sports activities

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('customer.authenticated.layouts.app')
 @section('main')
-    <div class="sports text-center"><h2 class="font-extrabold">Available Sports</h2></div>
+    <div class="sports text-center"><h2 class="font-extrabold">Available Sports Activities</h2></div>
     <div class="flex justify-between">
     @include('customer.authenticated.layouts.sports')
     </div>
